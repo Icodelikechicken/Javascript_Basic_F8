cf00570bf2b069b5c15fef5bda0f734b37b0b093
unknown <hoannissandr43@gmail.com>
2025-10-15 05:16:54 +0700

    Day 3

--- Function/main.js
+++ Function/main.js
@@ -1,2 +1,17 @@
-var cities = ["HaiPhong", "HaNoi", "TPHCM"]
-console.log(cities.join("-"))
+
+ 
+// Expected results:
+var arr = [
+    ['name', 'Sơn Đặng'],
+    ['age', 18],
+];
+
+function arrToObj(arr) {
+    var index = arr.reduce((accumulator, currentValue) => {
+        accumulator[currentValue[0]] = currentValue[1];
+        return accumulator;
+    }, {});
+    return index;
+}
+
+console.log(arrToObj(arr)); // { name: 'Sơn Đặng', age: 18 }
